017b16b87f62452ae1a56c3eb2c03246466c14b9
Emmanouil Palavras <44192780+epalavras@users.noreply.github.com>
2020-03-24 18:07:56 +0100

    add flyway

---
+++ src/main/resources/db/migration/V1.0__create_user_table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA extensions;
+
+CREATE TABLE skeleton.user (
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  , user_uuid UUID UNIQUE NOT NULL DEFAULT extensions.uuid_generate_v4()
+  , email TEXT UNIQUE NOT NULL
+  , password TEXT NOT NULL
+  , first_name TEXT NOT NULL
+  , last_name TEXT NOT NULL
+  , role TEXT NOT NULL
+);
---
+++ src/main/scala/com/skeleton/utils/database/FlywayService.scala
@@ -0,0 +1,42 @@
+package com.skeleton.utils.database
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.configuration.FluentConfiguration
+import org.flywaydb.core.internal.jdbc.DriverDataSource
+
+object FlywayService {
+
+  val config: Config = ConfigFactory.load()
+  val db: Config = config.getConfig("database")
+  val dbDriver: String = db.getString("driver")
+  val dbProps: Config = db.getConfig("properties")
+  val dbHost: String = dbProps.getString("serverName")
+  val dbPort: String = dbProps.getString("portNumber")
+  val dbName: String = dbProps.getString("databaseName")
+  val dbUrl: String = "jdbc:postgresql://"+dbHost+":"+dbPort+"/"+dbName
+  val dbUser: String = dbProps.getString("user")
+  val dbPass: String = dbProps.getString("password")
+
+  val dataSource = new DriverDataSource(Thread.currentThread.getContextClassLoader,
+    dbDriver,
+    dbUrl,
+    dbUser,
+    dbPass)
+
+  val flywayConfig: FluentConfiguration = Flyway
+    .configure()
+    .dataSource(dataSource)
+    .schemas("flyway_migrations", "extensions", "skeleton")
+
+  val flyway: Flyway = new Flyway(flywayConfig)
+
+  def migrate(): Unit = println("migrated: " + flyway.migrate())
+
+  def clean(): Unit = flyway.clean()
+
+  def recreate(): Unit = {
+    this.clean()
+    this.migrate()
+  }
+}
