63122437fff142ebcd8da50a826fd75010eafcfa
Emmanouil Palavras <44192780+epalavras@users.noreply.github.com>
2020-03-25 21:52:33 +0100

    add lazy val to plugins.sbt

--- src/main/scala/com/skeleton/utils/database/FlywayService.scala
+++ src/main/scala/com/skeleton/utils/database/FlywayService.scala
@@ -1,28 +1,24 @@
 package com.skeleton.utils.database
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{ Config, ConfigFactory }
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.flywaydb.core.internal.jdbc.DriverDataSource
 
 object FlywayService {
 
-  val config: Config = ConfigFactory.load()
-  val db: Config = config.getConfig("database")
+  val config: Config   = ConfigFactory.load()
+  val db: Config       = config.getConfig("database")
   val dbDriver: String = db.getString("driver")
-  val dbProps: Config = db.getConfig("properties")
-  val dbHost: String = dbProps.getString("serverName")
-  val dbPort: String = dbProps.getString("portNumber")
-  val dbName: String = dbProps.getString("databaseName")
-  val dbUrl: String = "jdbc:postgresql://"+dbHost+":"+dbPort+"/"+dbName
-  val dbUser: String = dbProps.getString("user")
-  val dbPass: String = dbProps.getString("password")
+  val dbProps: Config  = db.getConfig("properties")
+  val dbHost: String   = dbProps.getString("serverName")
+  val dbPort: String   = dbProps.getString("portNumber")
+  val dbName: String   = dbProps.getString("databaseName")
+  val dbUrl: String    = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName
+  val dbUser: String   = dbProps.getString("user")
+  val dbPass: String   = dbProps.getString("password")
 
-  val dataSource = new DriverDataSource(Thread.currentThread.getContextClassLoader,
-    dbDriver,
-    dbUrl,
-    dbUser,
-    dbPass)
+  val dataSource = new DriverDataSource(Thread.currentThread.getContextClassLoader, dbDriver, dbUrl, dbUser, dbPass)
 
   val flywayConfig: FluentConfiguration = Flyway
     .configure()
